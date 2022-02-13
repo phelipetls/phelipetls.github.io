816fff666e91d2e89b77cc303d224766b5992b73
phelipetls <phelipetls@users.noreply.github.com>
2022-02-13 15:34:04 +0000

    deploy: phelipetls/blog@852c1748f48c811d9aa82b68fdabcead8d90dbfc

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,18 @@
-const navContainer = document.querySelector(".nav-container");
+const navContainer = document.querySelector('.nav-container')
 
-let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-let lastScrollDirection = 0;
+let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop
+let lastScrollDirection = 0
 
 function handleScroll() {
-  const newScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const newScrollTop = window.pageYOffset || document.documentElement.scrollTop
 
   const scrollDirection = Math.sign(newScrollTop - lastScrollTop)
 
   if (scrollDirection !== lastScrollDirection) {
     if (scrollDirection === 1) {
-      navContainer.setAttribute('data-is-scrolling', 'down')
+      navContainer.classList.add('is-scrolling-down')
     } else if (scrollDirection === -1) {
-      navContainer.setAttribute('data-is-scrolling', 'up')
+      navContainer.classList.remove('is-scrolling-down')
     }
   }
 
@@ -23,9 +23,9 @@ window.addEventListener('scroll', handleScroll)
 
 const observer = new IntersectionObserver(
   function ([e]) {
-    e.target.classList.toggle("stuck", e.intersectionRatio < 1);
+    e.target.classList.toggle('stuck', e.intersectionRatio < 1)
   },
-  { rootMargin: "-1px 0px 0px 0px", threshold: [1] }
-);
+  { rootMargin: '-1px 0px 0px 0px', threshold: [1] }
+)
 
-observer.observe(navContainer);
+observer.observe(navContainer)
