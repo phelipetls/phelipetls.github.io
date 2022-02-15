b593b0ca74840e1aca9ce3db5215e5547bfc7628
phelipetls <phelipetls@users.noreply.github.com>
2022-02-15 03:44:28 +0000

    deploy: phelipetls/blog@d9b59772ffee926502b76050aa64d4eba273a347

--- js/nav.js
+++ js/nav.js
@@ -176,7 +176,9 @@
   function handleScroll() {
     const newScrollPosition = window.scrollY;
     if (newScrollPosition > lastScrollPosition) {
-      navContainer.classList.add("is-scrolling-down");
+      if (navContainer.classList.contains("stuck")) {
+        navContainer.classList.add("is-scrolling-down");
+      }
     } else {
       navContainer.classList.remove("is-scrolling-down");
     }
