c6030a174c2a4bca071ba67d53d7e52609aa49d9
phelipetls <phelipetls@users.noreply.github.com>
2023-04-07 23:34:14 +0000

    deploy: phelipetls/blog@6087e7a81b86ce4ed7761b8a4fe64a61cbcbc598

--- _astro/_BugVanillaSandpack.f9ab035d.js
+++ _astro/_BugVanillaSandpack.f9ab035d.js
@@ -1,4 +1,4 @@
-import{S as e,d as t}from"./index.5ad9caa2.js";import{j as a}from"./jsx-runtime.7a8c40fc.js";import"./index.57fae3ab.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.e3ea653a.js";import"./createReactComponent.099c7328.js";import"./CopyCodeBlockButton.ce6a0b11.js";import"./index.ffa87b56.js";import"./floating-ui.dom.browser.min.8b9d8992.js";function d(){return a.jsx(e,{template:"vanilla",title:"The same bug observed when using details element can be reproduced with vanilla JavaScript",files:{"/src/index.js":t`
+import{S as e,d as t}from"./index.440098a3.js";import{j as a}from"./jsx-runtime.7a8c40fc.js";import"./index.57fae3ab.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.e3ea653a.js";import"./createReactComponent.099c7328.js";import"./CopyCodeBlockButton.ce6a0b11.js";import"./index.ffa87b56.js";import"./floating-ui.dom.browser.min.8b9d8992.js";function d(){return a.jsx(e,{template:"vanilla",title:"The same bug observed when using details element can be reproduced with vanilla JavaScript",files:{"/src/index.js":t`
            document.getElementById('app').innerHTML = \`
            <span>State: closed</span>
 
--- _astro/_FormWithoutHelperTextSandpack.fa1c1956.js
+++ _astro/_FormWithoutHelperTextSandpack.fa1c1956.js
@@ -1,4 +1,4 @@
-import{S as e,d as r}from"./index.5ad9caa2.js";import{j as t}from"./jsx-runtime.7a8c40fc.js";import"./index.57fae3ab.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.e3ea653a.js";import"./createReactComponent.099c7328.js";import"./CopyCodeBlockButton.ce6a0b11.js";import"./index.ffa87b56.js";import"./floating-ui.dom.browser.min.8b9d8992.js";function d(){return t.jsx(e,{template:"react",files:{"/App.js":r`
+import{S as e,d as t}from"./index.440098a3.js";import{j as r}from"./jsx-runtime.7a8c40fc.js";import"./index.57fae3ab.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.e3ea653a.js";import"./createReactComponent.099c7328.js";import"./CopyCodeBlockButton.ce6a0b11.js";import"./index.ffa87b56.js";import"./floating-ui.dom.browser.min.8b9d8992.js";function d(){return r.jsx(e,{template:"react",files:{"/App.js":t`
            import { useState } from 'react'
 
            export default function App() {
@@ -25,15 +25,11 @@ import{S as e,d as r}from"./index.5ad9caa2.js";import{j as t}from"./jsx-runtime.
                      name="username"
                      value={username}
                      onChange={(e) => setUsername(e.target.value)}
-                     aria-invalid={errorMessage !== '' ? 'true' : 'false'}
-                     aria-errormessage="username-errormessage"
-                     aria-describedby="username-description"
                    />
                  </label>
 
                  {errorMessage && (
                    <div
-                     id="username-errormessage"
                      style={{
                        color: 'red',
                      }}
@@ -43,7 +39,6 @@ import{S as e,d as r}from"./index.5ad9caa2.js";import{j as t}from"./jsx-runtime.
                  )}
 
                  <div
-                   id="username-description"
                    style={{
                      fontSize: 10,
                      color: 'grey',
