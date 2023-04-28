cc3c07d95409d47b0d6a5459b760279d27042beb
phelipetls <phelipetls@users.noreply.github.com>
2023-04-28 21:22:28 +0000

    deploy: phelipetls/blog@03b209548fc2563c3065c26752b50c40a3ed4131

--- _astro/_GoodFormSandpack.485daa58.js
+++ _astro/_GoodFormSandpack.485daa58.js
@@ -1,4 +1,4 @@
-import{S as t}from"./Sandpack.11d3eb3b.js";import{d as e}from"./index.8aa7c818.js";import{j as r}from"./jsx-runtime.0673f7bd.js";import"./index.57fae3ab.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.a9c2365b.js";import"./CopyCodeBlockButton.55bfe2f1.js";import"./index.ffa87b56.js";import"./createLucideIcon.59f306f6.js";import"./floating-ui.dom.browser.min.0e955118.js";function f(){return r.jsx(t,{template:"react",files:{"/App.js":e`
+import{S as t}from"./Sandpack.ddf6e799.js";import{d as e}from"./index.8aa7c818.js";import{j as r}from"./jsx-runtime.0673f7bd.js";import"./index.57fae3ab.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.a9c2365b.js";import"./CopyCodeBlockButton.55bfe2f1.js";import"./index.ffa87b56.js";import"./createLucideIcon.59f306f6.js";import"./floating-ui.dom.browser.min.0e955118.js";function f(){return r.jsx(t,{template:"react",files:{"/App.js":e`
            export default function App() {
              const handleSubmit = (e) => {
                e.preventDefault()
--- _astro/_InfiniteLoopBugSandpack.6207ba3a.js
+++ _astro/_InfiniteLoopBugSandpack.6207ba3a.js
@@ -1,4 +1,4 @@
-import{S as t}from"./Sandpack.11d3eb3b.js";import{d as e}from"./index.8aa7c818.js";import{j as o}from"./jsx-runtime.0673f7bd.js";import"./index.57fae3ab.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.a9c2365b.js";import"./CopyCodeBlockButton.55bfe2f1.js";import"./index.ffa87b56.js";import"./createLucideIcon.59f306f6.js";import"./floating-ui.dom.browser.min.0e955118.js";function d(){return o.jsx(t,{template:"react",title:"Infinite loop when using details toggle event",files:{"App.js":e`
+import{S as t}from"./Sandpack.ddf6e799.js";import{d as e}from"./index.8aa7c818.js";import{j as o}from"./jsx-runtime.0673f7bd.js";import"./index.57fae3ab.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.a9c2365b.js";import"./CopyCodeBlockButton.55bfe2f1.js";import"./index.ffa87b56.js";import"./createLucideIcon.59f306f6.js";import"./floating-ui.dom.browser.min.0e955118.js";function d(){return o.jsx(t,{template:"react",title:"Infinite loop when using details toggle event",files:{"App.js":e`
           import { useState } from 'react'
 
           export default function App() {
