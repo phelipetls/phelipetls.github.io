f5baae5d30968b0a48fe71c9c4c079b8eb6949db
phelipetls <phelipetls@users.noreply.github.com>
2023-06-14 22:46:11 +0000

    deploy: phelipetls/blog@b5d4dbfc8373d3b350dff065d41b58b883b682b9

--- _astro/_FormWithoutHelperTextSandpack.55e59e3a.js
+++ _astro/_FormWithoutHelperTextSandpack.55e59e3a.js
@@ -1,4 +1,4 @@
-import{S as e}from"./Sandpack.46b9e468.js";import{d as r}from"./index.8aa7c818.js";import{j as t}from"./jsx-runtime.a3ab6fc0.js";import"./index.c0d9332e.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.28520c38.js";import"./CopyCodeBlockButton.83b14759.js";import"./index.772d13e4.js";import"./createLucideIcon.dae7146d.js";import"./floating-ui.dom.browser.min.b9202534.js";function c(){return t.jsx(e,{template:"react",files:{"/App.js":r`
+import{S as e}from"./Sandpack.2848b8bf.js";import{d as t}from"./index.8aa7c818.js";import{j as r}from"./jsx-runtime.a3ab6fc0.js";import"./index.c0d9332e.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.28520c38.js";import"./CopyCodeBlockButton.83b14759.js";import"./index.772d13e4.js";import"./createLucideIcon.dae7146d.js";import"./floating-ui.dom.browser.min.b9202534.js";function f(){return r.jsx(e,{template:"react",files:{"/App.js":t`
            import { useState } from 'react'
 
            export default function App() {
@@ -25,15 +25,11 @@ import{S as e}from"./Sandpack.46b9e468.js";import{d as r}from"./index.8aa7c818.j
                      name="username"
                      value={username}
                      onChange={(e) => setUsername(e.target.value)}
-                     aria-invalid={errorMessage !== '' ? 'true' : 'false'}
-                     aria-errormessage="username-errormessage"
-                     aria-describedby="username-description"
                    />
                  </label>
 
                  {errorMessage && (
                    <div
-                     id="username-errormessage"
                      style={{
                        color: 'red',
                      }}
@@ -43,7 +39,6 @@ import{S as e}from"./Sandpack.46b9e468.js";import{d as r}from"./index.8aa7c818.j
                  )}
 
                  <div
-                   id="username-description"
                    style={{
                      fontSize: 10,
                      color: 'grey',
@@ -56,4 +51,4 @@ import{S as e}from"./Sandpack.46b9e468.js";import{d as r}from"./index.8aa7c818.j
                </form>
              )
            }
-        `}})}export{c as default};
+        `}})}export{f as default};
---
+++ _astro/_UrlSearchParamZodExample.842dd5cf.js
@@ -0,0 +1,12 @@
+import{S as e}from"./Sandpack.2848b8bf.js";import{j as r}from"./jsx-runtime.a3ab6fc0.js";import"./index.c0d9332e.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.28520c38.js";import"./CopyCodeBlockButton.83b14759.js";import"./index.772d13e4.js";import"./createLucideIcon.dae7146d.js";import"./floating-ui.dom.browser.min.b9202534.js";function c(){return r.jsx(e,{template:"react",title:"An example of parsing a URL search param with Zod",initialURL:"/?pageNumber=3",shouldShowNavigator:!0,customSetup:{dependencies:{zod:"^3.21.4"}},files:{"App.js":{code:`import z from 'zod'
+import * as React from 'react'
+
+export default function App() {
+  const searchParams = new URLSearchParams(window.location.search)
+
+  const pageNumberRaw = searchParams.get('pageNumber')
+  const pageNumber = z.number().catch(0).parse(Number(pageNumberRaw))
+
+  return <pre>{JSON.stringify({ pageNumber, pageNumberRaw }, null, 2)}</pre>
+}
+`}}})}export{c as default};
