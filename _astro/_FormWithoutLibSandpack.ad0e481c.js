5075b63f1e27022e81ba7c47728831f7d57d111b
phelipetls <phelipetls@users.noreply.github.com>
2023-08-06 15:06:13 +0000

    deploy: phelipetls/blog@af21e9dc1e7f72356a3538d2416e7c372a753180

--- _astro/_FormWithoutLibSandpack.ad0e481c.js
+++ _astro/_FormWithoutLibSandpack.ad0e481c.js
@@ -1,4 +1,4 @@
-import{S as e}from"./Sandpack.d2e57330.js";import{d as t}from"./index.8aa7c818.js";import{j as a}from"./jsx-runtime.a3ab6fc0.js";import"./index.c0d9332e.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.28520c38.js";import"./CopyCodeBlockButton.76371dc9.js";import"./createLucideIcon.927af0e3.js";function s(){return a.jsx(e,{template:"react",files:{"/App.js":t`
+import{S as e}from"./Sandpack.d0088744.js";import{d as t}from"./index.8aa7c818.js";import{j as a}from"./jsx-runtime.a3ab6fc0.js";import"./index.c0d9332e.js";import"./_commonjsHelpers.042e6b4d.js";import"./Tab.28520c38.js";import"./CopyCodeBlockButton.7cde34a4.js";import"./createLucideIcon.39155ea3.js";function s(){return a.jsx(e,{template:"react",files:{"/App.js":t`
            export default function App() {
              const handleSubmit = (e) => {
                e.preventDefault()
