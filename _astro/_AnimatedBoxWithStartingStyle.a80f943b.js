0b575f9163f012552a6ab2e73c35d03096ffb7d2
phelipetls <phelipetls@users.noreply.github.com>
2023-11-19 15:25:56 +0000

    deploy: phelipetls/blog@d49cff9726724c4058424d7ecfb5fb675b228a65

---
+++ _astro/_AnimatedBoxWithStartingStyle.a80f943b.js
@@ -0,0 +1,22 @@
+import{j as t}from"./tw-merge.dd1f01ef.js";import{S as a}from"./Sandpack.f1ec7202.js";import"./index.6460afdd.js";import"./Tab.497d6dce.js";import"./CopyCodeBlockButton.cb5c320d.js";import"./createLucideIcon.cbcd067e.js";function m(){return t.jsx(a,{template:"static",title:"Animating a box moving forward (with CSS transitions and @starting-style)",files:{"/index.html":{active:!0,code:`<!DOCTYPE html>
+<html>
+  <style>
+    .box {
+      background-color: black;
+      width: 20px;
+      aspect-ratio: 1 / 1;
+      transition: transform 1s linear;
+      transform: translateX(100px);
+    }
+
+    @starting-style {
+      .box {
+        transform: translateX(0px);
+      }
+    }
+  </style>
+
+  <body>
+    <div class="box"></div>
+  </body>
+<html>`}}})}export{m as default};
--- _astro/_DebounceReactNaiveButtonExample.60d0417c.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./tw-merge.dd1f01ef.js";import{S as o}from"./Sandpack.7ab693b2.js";import"./index.6460afdd.js";import"./Tab.497d6dce.js";import"./CopyCodeBlockButton.08bbc0f2.js";import"./IconButton.9e457559.js";function p(e){return t.jsx(o,{...e,title:"Implementing debounced event handler in React in a naive way",template:"react",files:{"/App.js":`import { debounce } from './debounce.js'
-
-export default function App() {
-  return <button onClick={debounce(() => console.log('I ran'), 300)}>Click me</button>
-}
-`,"/debounce.js":e.debounceCode}})}export{p as default};
