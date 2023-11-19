4facff2e010ae8e504f69cdf76484a95ea61bda4
phelipetls <phelipetls@users.noreply.github.com>
2023-11-19 12:23:27 +0000

    deploy: phelipetls/blog@2c14662f46ccc3758e41d1dca8ec3ab1e5aa9725

---
+++ _astro/_AnimatedBoxWithCssTransition.373a99b4.js
@@ -0,0 +1,25 @@
+import{j as t}from"./tw-merge.dd1f01ef.js";import{S as o}from"./Sandpack.7ab693b2.js";import"./index.6460afdd.js";import"./Tab.497d6dce.js";import"./CopyCodeBlockButton.08bbc0f2.js";import"./IconButton.9e457559.js";function m(){return t.jsx(o,{template:"static",title:"Animating a box moving forward (with CSS transitions)",files:{"/index.html":`<!DOCTYPE html>
+<html>
+  <style>
+    .box {
+      background-color: black;
+      width: 20px;
+      aspect-ratio: 1 / 1;
+      transition: transform 1s linear;
+    }
+
+    .animated-box {
+      transform: translateX(100px);
+    }
+  </style>
+
+  <body>
+    <div class="box"></div>
+  </body>
+
+  <script>
+    // We need to force a reflow here for the animation to work
+    document.body.getBoundingClientRect()
+    document.querySelector('.box').classList.add('animated-box')
+  <\/script>
+</html>`}})}export{m as default};
---
+++ _astro/_BugReactSandpack.1b8821bc.js
@@ -0,0 +1,24 @@
+import{j as t}from"./tw-merge.dd1f01ef.js";import{S as e}from"./Sandpack.7ab693b2.js";import{d as s}from"./index.8aa7c818.js";import"./index.6460afdd.js";import"./Tab.497d6dce.js";import"./CopyCodeBlockButton.08bbc0f2.js";import"./IconButton.9e457559.js";function u(){return t.jsx(e,{template:"react",title:"Bug when using details element with React",files:{"App.js":s`
+          import { useState } from 'react'
+
+          export default function App() {
+            const [isOpen, setIsOpen] = useState(false)
+
+            return (
+              <>
+                <span>State: {isOpen ? 'open' : 'closed'}</span>
+
+                <details open={isOpen}>
+                  <summary
+                    onClick={() => {
+                      setIsOpen(!isOpen)
+                    }}
+                  >
+                    Summary
+                  </summary>
+                  Details
+                </details>
+              </>
+            )
+          }
+        `}})}export{u as default};
