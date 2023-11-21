287a5ded190375aca7912a78a33204c2d52bcae5
phelipetls <phelipetls@users.noreply.github.com>
2023-11-21 00:58:56 +0000

    deploy: phelipetls/blog@3b96daf3bce35710a31667d9aba6271457e0acb4

---
+++ _astro/_AnimatedBox.ba67c722.js
@@ -0,0 +1,24 @@
+import{j as o}from"./tw-merge.dd1f01ef.js";import{S as i}from"./Sandpack.f1ec7202.js";import"./index.6460afdd.js";import"./Tab.497d6dce.js";import"./CopyCodeBlockButton.cb5c320d.js";import"./createLucideIcon.cbcd067e.js";function x({withForwards:t=!0,...a}){return o.jsx(i,{template:"static",...a,title:"Animating a box moving forward",files:{"/index.html":`<!DOCTYPE html>
+<html>
+  <style>
+    .box {
+      background-color: black;
+      width: 20px;
+      aspect-ratio: 1 / 1;
+    }
+
+    @keyframes box-animation {
+      to {
+        transform: translateX(100px);
+      }
+    }
+
+    .animated-box {
+      animation: box-animation 1s linear${t?" forwards":""};
+    }
+  </style>
+
+  <body>
+    <div class="box animated-box"></div>
+  </body>
+</html>`}})}export{x as default};
