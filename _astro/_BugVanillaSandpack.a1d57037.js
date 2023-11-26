03a416a527b7398f78fba5e27f9aadb7cafae050
phelipetls <phelipetls@users.noreply.github.com>
2023-11-26 13:47:49 +0000

    deploy: phelipetls/blog@cbc64b7723990f7d8a8e61bfe30fe33e333a0f10

---
+++ _astro/_BugVanillaSandpack.a1d57037.js
@@ -0,0 +1,29 @@
+import{j as e}from"./tw-merge.dd1f01ef.js";import{S as t}from"./Sandpack.3493e1c6.js";import{d as a}from"./index.8aa7c818.js";import"./index.6460afdd.js";import"./Tab.497d6dce.js";import"./CopyCodeBlockButton.1edd83cb.js";import"./createLucideIcon.82e5ca66.js";function p(){return e.jsx(t,{template:"vanilla",title:"The same bug observed when using details element can be reproduced with vanilla JavaScript",files:{"index.js":a`
+           document.getElementById('app').innerHTML = \`
+           <span>State: closed</span>
+
+           <details>
+             <summary>Summary</summary>
+             Details
+           </details>
+           \`
+
+           const span = document.querySelector('span')
+           const details = document.querySelector('details')
+           const summary = document.querySelector('summary')
+
+           let isOpen = false
+
+           summary.addEventListener('click', () => {
+             if (isOpen) {
+               isOpen = false
+               span.textContent = 'State: closed'
+               details.removeAttribute('open')
+               return
+             }
+
+             isOpen = true
+             span.textContent = 'State: open'
+             details.setAttribute('open', '')
+           })
+        `}})}export{p as default};
---
+++ _astro/_GoodFormSandpack.28dc203b.js
@@ -0,0 +1,20 @@
+import{j as e}from"./tw-merge.dd1f01ef.js";import{S as t}from"./Sandpack.3493e1c6.js";import{d as r}from"./index.8aa7c818.js";import"./index.6460afdd.js";import"./Tab.497d6dce.js";import"./CopyCodeBlockButton.1edd83cb.js";import"./createLucideIcon.82e5ca66.js";function l(){return e.jsx(t,{template:"react",files:{"/App.js":r`
+           export default function App() {
+             const handleSubmit = (e) => {
+               e.preventDefault()
+               const username = e.target.elements['username'].value
+               alert(\`Ok, \${username}, I sent your data to our server ✨\`)
+             }
+
+             return (
+               <form onSubmit={handleSubmit}>
+                 <label>
+                   What's your name?
+                   <input name="username" placeholder="Enter your name..." />
+                 </label>
+
+                 <button>Submit</button>
+               </form>
+             )
+           }
+        `}})}export{l as default};
